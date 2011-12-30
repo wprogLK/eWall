aec50d03f4a7fc2d95ddce665143bb99012091c7
Lukas Keller <lukas.keller@solnet.ch>
2011-12-30 19:56:29 +0100

    home of user & navigation
    	modified:   app/controllers/UserController.java
    	modified:   app/views/Application/index.html
    	new file:   app/views/UserController/home.html
    	modified:   app/views/header.html
    	modified:   app/views/main.html
    	modified:   app/views/navigation.html

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,10 +1,16 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Secure.class)
 public class UserController extends Controller
 {
-	//TODO
+	public static void home()
+	{
+		String username = Secure.Security.connected();
+    	User currentUser = User.find("byUsername", username).first();
+		render(currentUser);
+	}
 }
